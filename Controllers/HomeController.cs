34491a5d91435d0e7fb9a08bfd608be4db0d5685
Paras Panchal <paraspanchal5555@gmail.com>
2024-03-01 23:38:13 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using UploadFile.Models;
+
+namespace UploadFile.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            MultipleFilesModel model = new MultipleFilesModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Index(MultipleFilesModel model)
+        {
+           // if (ModelState.IsValid)
+            //{
+                model.IsResponse = true;
+                if (model.Files.Count > 0)
+                {
+                    foreach (var file in model.Files)
+                    {
+
+                        string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Files");
+
+                        //create folder if not exist
+                        if (!Directory.Exists(path))
+                            Directory.CreateDirectory(path);
+
+
+                        string fileNameWithPath = Path.Combine(path, file.FileName);
+
+                        using (var stream = new FileStream(fileNameWithPath, FileMode.Create))
+                        {
+                            file.CopyTo(stream);
+                        }
+                    }
+                    model.IsSuccess = true;
+                    model.Message = "Files upload successfully";
+                }
+                else
+                {
+                    model.IsSuccess = false;
+                    model.Message = "Please select files";
+                }
+            //}
+            return View("Index", model);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/MultipleFilesModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UploadFile.Models
+{
+    public class MultipleFilesModel : ReponseModel
+    {
+        [Required(ErrorMessage = "Please select files")]
+        public List<IFormFile> Files { get; set; }
+    }
+}
